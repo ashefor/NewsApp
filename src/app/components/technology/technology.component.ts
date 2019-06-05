ea0363733f077b201d5951ed5eb50a81a5fbd4b5
ashefor <thatsureboy@gmail.com>
2019-06-05 15:01:15 +0100

    create a new page for technology category

---
+++ src/app/components/technology/technology.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { NewsapiService } from 'src/app/services/newsapi.service';
+
+@Component({
+  selector: 'app-technology',
+  templateUrl: './technology.component.html',
+  styleUrls: ['./technology.component.scss']
+})
+export class TechnologyComponent implements OnInit {
+  page = 1;
+  pageSize = 20;
+  allTechNews: any[]= [];
+  length;
+  color = 'primary';
+  mode = 'indeterminate';
+  pageloader = true;
+  mobileSpinner = true;
+  showPaginator = false;
+
+  constructor(private service: NewsapiService) { }
+
+  ngOnInit() {
+    this.fetchAllTechNews(this.page, this.pageSize)
+  }
+
+  fetchAllTechNews(page, pageSize){
+    this.service.getAllTechNews(page,pageSize).subscribe((res:any)=>{
+      console.log(res)
+      if(res.status === 'ok'){
+        this.allTechNews = res.articles;
+        this.length = res.totalResults;
+        this.pageloader = false;
+        this.mobileSpinner = false;
+        this.showPaginator = true;
+      }
+    })
+  }
+
+  paginator(event){
+    const pageIndex = event.pageIndex+1;
+    const pageSize = event.pageSize;
+    this.mobileSpinner = true;
+    this.service.getAllTechNews(pageIndex, pageSize).subscribe((res: any)=>{
+      this.allTechNews = res.articles;
+      this.mobileSpinner = false;
+    })
+  }
+}
